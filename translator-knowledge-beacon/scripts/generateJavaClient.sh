4f8a194293a48b275a482f9355cec3942a822fcd
Richard Bruskiewich <richard@starinformatics.com>
2017-04-28 09:50:04 -0700

    Fixing bash scripts for Mac

--- translator-knowledge-beacon/scripts/generateJavaClient.sh
+++ translator-knowledge-beacon/scripts/generateJavaClient.sh
@@ -8,11 +8,24 @@
 #  Note that the SWAGGER_CODEGEN_PATH environment variable should point
 #  to the folder where your downloaded swagger-codegen-cli.jar is located.
 #
+if [ -z $SWAGGER_CODEGEN_PATH ]; then
+	# current directory is default if no path given?
+	SWAGGER_CODEGEN_PATH="."
+	echo "Swagger Codegen JAR path set to '$SWAGGER_CODEGEN_PATH'"
+fi
+
+SWAGGER_CLI=`which swagger-codegen-cli`
+if [ ! -e $SWAGGER_CLI ]; then
+	SWAGGER_CLI=java -jar $SWAGGER_CODEGEN_PATH/swagger-codegen-cli.jar
+fi
+
+echo "Swagger CLI executable is '$SWAGGER_EXEC'"
+
 
 if [ "$#" -lt 1 ]; then
 	echo "generateJavaClient.sh <path to specification file>"
 else
-	java -jar $SWAGGER_CODEGEN_PATH/swagger-codegen-cli.jar generate	\
+	$SWAGGER_EXEC generate	\
 	-i $1						\
 	-l java						\
 	-o ../client					\
