c926a9e6a1f4dba31b0488362f5d5542a8503db7
Richard Bruskiewich <richard@starinformatics.com>
2017-04-28 10:05:41 -0700

    Iteration on Mac script

--- translator-knowledge-beacon/scripts/generateJavaClient.sh
+++ translator-knowledge-beacon/scripts/generateJavaClient.sh
@@ -14,18 +14,18 @@ if [ -z $SWAGGER_CODEGEN_PATH ]; then
 	echo "Swagger Codegen JAR path set to '$SWAGGER_CODEGEN_PATH'"
 fi
 
-SWAGGER_CLI=`which swagger-codegen-cli`
-if [ ! -e $SWAGGER_CLI ]; then
-	SWAGGER_CLI=java -jar $SWAGGER_CODEGEN_PATH/swagger-codegen-cli.jar
+SWAGGER_CODEGEN_EXEC=`which swagger-codegen`
+if [ ! -e $SWAGGER_CODEGEN_EXEC ]; then
+	SWAGGER_CODEGEN_EXEC=java -jar $SWAGGER_CODEGEN_PATH/swagger-codegen-cli.jar
 fi
 
-echo "Swagger CLI executable is '$SWAGGER_EXEC'"
+echo "Swagger Codegen executable is '$SWAGGER_CODEGEN_EXEC'"
 
 
 if [ "$#" -lt 1 ]; then
 	echo "generateJavaClient.sh <path to specification file>"
 else
-	$SWAGGER_EXEC generate	\
+	$SWAGGER_CODEGEN_EXEC generate	\
 	-i $1						\
 	-l java						\
 	-o ../client					\
